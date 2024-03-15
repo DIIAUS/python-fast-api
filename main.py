c1a23b975bf28cf3e30f750f6077eeebb8ab295b
Nathan <natan.b@ku.th>
2024-03-15 15:11:05 +0700

    basic

---
+++ main.py
@@ -0,0 +1,18 @@
+from typing import Union
+from fastapi import FastAPI
+from models.itemModel import Item
+
+app = FastAPI(title='My API', version='0.1.0', summary='My first fastapi app' )
+
+
+@app.get("/")
+async def read_root():
+    return "this is python fast api"
+
+@app.get("/items/{itemId}", tags=['items'])
+async def read_item(itemId: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{itemId}", response_model = Item, tags=['items'])
+def updateItem(itemId: int, item: Item):
+    return {"itemName": item.name, "itemId": itemId}
---
+++ routes/itemRoute.py
@@ -0,0 +1,3 @@
+from typing import Union
+from fastapi import FastAPI
+from models.itemModel import Item
